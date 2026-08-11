98e0444c8287d5c32ece5e205643f480931d1d6c
alfredoloai05 <140649105+alfredoloai05@users.noreply.github.com>
2026-08-11 12:45:38 -0500

    feat: enable delivery risk radar

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './enhancements.css';
+import './riskEnhancer.css';
+import './riskEnhancer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
