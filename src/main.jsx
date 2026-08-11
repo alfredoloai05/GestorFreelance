48be0ad8da4544b788f01e4ad4eeb30599e643b3
alfredoloai05 <140649105+alfredoloai05@users.noreply.github.com>
2026-08-11 16:49:10 -0500

    feat: load personal finance workspace

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './styles.css';
 import './enhancements.css';
 import './riskEnhancer.css';
 import './riskEnhancer';
+import './FinancePortal';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
