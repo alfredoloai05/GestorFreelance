60137d7cc1a0abe40c228c92270998480857ce57
Alfredo Loaiza <140649105+alfredoloai05@users.noreply.github.com>
2026-08-14 17:45:12 -0500

    style: load Jarvis assistant polish

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import './jarvisPolish.css';
 import './calendarPolish.css';
 import './projectModalPro.css';
 import './taskTablePolish.css';
+import './assistantPolish.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
